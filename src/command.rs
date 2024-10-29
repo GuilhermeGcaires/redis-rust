af38f467613de14a39f14cccedcaf4b9f0317a42
guilhermegcaires <guilhermegcaires@gmail.com>
2024-10-28 22:11:27 -0300

    Changed on how commands are parsed

--- src/command.rs
+++ src/command.rs
@@ -15,55 +15,67 @@ pub enum Command {
 
 impl Command {
     pub fn from_resp(resp: Vec<RespType>) -> Command {
-        println!("Resp vector: {:?}", resp.get(0));
+        println!("Resp vector: {:?}", resp);
 
-        if let Some(RespType::BulkString(command)) = resp.get(0) {
-            match command.to_lowercase().as_str() {
-                "ping" => Command::Ping,
-                "echo" => {
-                    if let Some(RespType::BulkString(arg)) = resp.get(1) {
-                        Command::Echo(arg.clone())
-                    } else {
-                        Command::Unknown
-                    }
-                }
-                "set" => {
-                    let key = resp.get(1).and_then(|x| {
-                        if let RespType::BulkString(s) = x {
-                            Some(s.clone())
-                        } else {
-                            None
-                        }
-                    });
-                    let value = resp.get(2).and_then(|x| {
-                        if let RespType::BulkString(s) = x {
-                            Some(s.clone())
-                        } else {
-                            None
-                        }
-                    });
-                    let ttl = resp.get(3).and_then(|x| {
-                        if let RespType::BulkString(s) = x {
-                            s.parse::<u64>().ok()
-                        } else {
-                            None
-                        }
-                    });
+        if let Some(RespType::Array(inner_resp)) = resp.get(0) {
+            if inner_resp.is_empty() {
+                return Command::Unknown;
+            }
 
-                    Command::Set {
-                        key: key.unwrap_or_default(),
-                        value: value.unwrap_or_default(),
-                        ttl,
+            if let RespType::BulkString(command) = &inner_resp[0] {
+                match command.to_lowercase().as_str() {
+                    "ping" => Command::Ping,
+                    "echo" => {
+                        if let Some(RespType::BulkString(arg)) = inner_resp.get(1) {
+                            Command::Echo(arg.clone())
+                        } else {
+                            Command::Unknown
+                        }
                     }
-                }
-                "get" => {
-                    if let Some(RespType::BulkString(key)) = resp.get(1) {
-                        Command::Get(key.clone())
-                    } else {
-                        Command::Unknown
+                    "set" => {
+                        let key = inner_resp
+                            .get(1)
+                            .and_then(|x| {
+                                if let RespType::BulkString(s) = x {
+                                    Some(s.clone())
+                                } else {
+                                    None
+                                }
+                            })
+                            .unwrap_or_default();
+
+                        let value = inner_resp
+                            .get(2)
+                            .and_then(|x| {
+                                if let RespType::BulkString(s) = x {
+                                    Some(s.clone())
+                                } else {
+                                    None
+                                }
+                            })
+                            .unwrap_or_default();
+
+                        let ttl = inner_resp.get(3).and_then(|x| {
+                            if let RespType::BulkString(s) = x {
+                                s.parse::<u64>().ok()
+                            } else {
+                                None
+                            }
+                        });
+
+                        Command::Set { key, value, ttl }
                     }
+                    "get" => {
+                        if let Some(RespType::BulkString(key)) = inner_resp.get(1) {
+                            Command::Get(key.clone())
+                        } else {
+                            Command::Unknown
+                        }
+                    }
+                    _ => Command::Unknown,
                 }
-                _ => Command::Unknown,
+            } else {
+                Command::Unknown
             }
         } else {
             Command::Unknown
