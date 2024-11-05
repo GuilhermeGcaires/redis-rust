e0db0d12983d92a3037c369dc53826e959fc709b
ggcaires <guilhermegcaires@gmail.com>
2024-11-05 11:21:17 -0300

    Changed where the rdb file is loaded

--- src/main.rs
+++ src/main.rs
@@ -118,76 +118,19 @@ async fn handle_client(mut stream: TcpStream, in_memory: &mut Arc<Mutex<Database
                     },
                     Command::Keys(_) => {
                         let mut db = in_memory.lock().unwrap();
-                        let mut path = db.config.dir.clone().unwrap();
-                        let file_name = db.config.dbfilename.clone().unwrap();
+                        let db_keys = db
+                            .storage
+                            .keys()
+                            .map(|key| RespType::BulkString(key.clone())) // Convert each `&String` to `RespType::BulkString`
+                            .collect::<Vec<RespType>>();
 
-                        path.push_str("/");
-                        path.push_str(&file_name);
-                        println!("File path: {:?}", path);
-
-                        let file = File::open(path);
-                        match file {
-                            Ok(file) => {
-                                let mut file_buffer: [u8; 1024] = [0; 1024];
-                                let mut reader = BufReader::new(file);
-                                reader.read(&mut file_buffer).unwrap();
-                                println!("Hex Buffer: {:x?}", file_buffer);
-                                println!("Buffer: {:?}", String::from_utf8_lossy(&file_buffer));
-                                let mut iterator =
-                                    file_buffer.iter().skip_while(|&b| *b != 0xfb).skip(1);
-
-                                let _size_hash_table = iterator.next();
-                                let _size_expire_hash_table = iterator.next();
-                                let _value_type = iterator.next();
-                                let key_len = *iterator.next().unwrap() as usize;
-                                let mut key_chars = Vec::with_capacity(key_len);
-                                for _ in 0..key_len {
-                                    if let Some(&byte) = iterator.next() {
-                                        key_chars.push(byte)
-                                    } else {
-                                        break;
-                                    }
-                                }
-
-                                let value_len = *iterator.next().unwrap() as usize;
-                                let mut value_chars = Vec::with_capacity(value_len);
-                                for _ in 0..value_len {
-                                    if let Some(&byte) = iterator.next() {
-                                        value_chars.push(byte)
-                                    } else {
-                                        break;
-                                    }
-                                }
-
-                                let key_string = String::from_utf8_lossy(&key_chars).to_string();
-                                let value_string =
-                                    String::from_utf8_lossy(&value_chars).to_string();
-
-                                let new_item = Item::new(value_string.clone(), None);
-
-                                db.storage.insert(key_string.clone(), new_item);
-
-                                println!("{in_memory:?}");
-
-                                let response =
-                                    RespType::Array(vec![RespType::BulkString(key_string)])
-                                        .serialize();
-                                println!("{response:?}");
-
-                                response
-                            }
-                            Err(_) => {
-                                println!("Falhou");
-                                RespType::Array(vec![]).serialize()
-                            }
-                        }
+                        RespType::Array(db_keys).serialize()
                     }
                     Command::Unknown => {
                         RespType::SimpleString("-ERR Unknown command\r\n".to_string()).serialize()
                     }
                 };
 
-                // Send the response back to the client
                 if let Err(e) = stream.write_all(response.as_bytes()).await {
                     eprintln!("Error sending response: {}", e);
                     break;
@@ -198,6 +141,68 @@ async fn handle_client(mut stream: TcpStream, in_memory: &mut Arc<Mutex<Database
                 break;
             }
         }
+    }
+}
+
+fn load_rdb_to_database(in_memory: Arc<Mutex<Database>>) {
+    let mut db = in_memory.lock().unwrap();
+
+    if db.config.dir == None || db.config.dbfilename == None {
+        return;
+    }
+
+    let mut path = db
+        .config
+        .dir
+        .clone()
+        .expect("Couldn't retrieve path of file");
+    let file_name = db.config.dbfilename.clone().unwrap();
+
+    path.push_str("/");
+    path.push_str(&file_name);
+    println!("File path: {:?}", path);
+
+    let file = File::open(path);
+    match file {
+        Ok(file) => {
+            let mut file_buffer: [u8; 1024] = [0; 1024];
+            let mut reader = BufReader::new(file);
+            reader.read(&mut file_buffer).unwrap();
+            println!("Hex Buffer: {:x?}", file_buffer);
+            println!("Buffer: {:?}", String::from_utf8_lossy(&file_buffer));
+            let mut iterator = file_buffer.iter().skip_while(|&b| *b != 0xfb).skip(1);
+
+            let _size_hash_table = iterator.next();
+            let _size_expire_hash_table = iterator.next();
+            let _value_type = iterator.next();
+            let key_len = *iterator.next().unwrap() as usize;
+            let mut key_chars = Vec::with_capacity(key_len);
+            for _ in 0..key_len {
+                if let Some(&byte) = iterator.next() {
+                    key_chars.push(byte)
+                } else {
+                    break;
+                }
+            }
+
+            let value_len = *iterator.next().unwrap() as usize;
+            let mut value_chars = Vec::with_capacity(value_len);
+            for _ in 0..value_len {
+                if let Some(&byte) = iterator.next() {
+                    value_chars.push(byte)
+                } else {
+                    break;
+                }
+            }
+
+            let key_string = String::from_utf8_lossy(&key_chars).to_string();
+            let value_string = String::from_utf8_lossy(&value_chars).to_string();
+
+            let new_item = Item::new(value_string.clone(), None);
+
+            db.storage.insert(key_string.clone(), new_item);
+        }
+        Err(_) => eprintln!("Couldn't find rdb file."),
     }
 }
 
@@ -213,9 +218,10 @@ async fn main() {
         }
         (None, None) => Config::new(None, None),
     };
+    let in_memory: Arc<Mutex<Database>> = Arc::new(Mutex::new(Database::new(config)));
+    load_rdb_to_database(Arc::clone(&in_memory));
 
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
-    let in_memory: Arc<Mutex<Database>> = Arc::new(Mutex::new(Database::new(config)));
     println!("{:?}", in_memory);
 
     loop {
